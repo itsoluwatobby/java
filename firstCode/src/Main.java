b475a9c5fd46c5d504037658be5c8c9ddba82de5
LOVELY-WANDYTE\personal <itsoluwatobby@gmail.com>
2024-06-29 05:37:43 +0100

    refactor(modification): modified mortagage calculator

--- firstCode/src/Main.java
+++ firstCode/src/Main.java
@@ -7,8 +7,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Exercise exercise = new Exercise();
-        exercise.exercise();
+//        Learning learning = new Learning();
+//        learning.learningPeriod();
+//        Exercise exercise = new Exercise();
+//        exercise.exercise();
+        MortgageCalculator mortgageCalculator = new MortgageCalculator();
+        mortgageCalculator.annualCalculator();
     }
 }
 
@@ -36,42 +40,92 @@ class Learning {
 //        System.out.println(className);
 
         //  SWITCH STATEMENT
-        String role = "";
-        switch (role) {
-            case "admin":
-                System.out.println("You're an Admin");
-                break;
-            case "moderator":
-                System.out.println("You're a Moderator");
-                break;
-            default:
-                System.out.println("You are a guest");
-        }
+//        String role = "";
+//        switch (role) {
+//            case "admin":
+//                System.out.println("You're an Admin");
+//                break;
+//            case "moderator":
+//                System.out.println("You're a Moderator");
+//                break;
+//            default:
+//                System.out.println("You are a guest");
+//        }
+
+//        LOOPS
+
+        String[] fruits = { "Apple", "Mango", "Orange" };
+        for (String fruit : fruits) System.out.println(fruit); // for each loop
     }
 }
 
-class AnnualCalculator {
+class MortgageCalculator {
+    final byte MONTHS_IN_YEAR = 12;
+    final byte PERCENT = 100;
+    final int MIN_PRINCIPAL = 1_000;
+    final int MAX_PRINCIPAL = 1_000_000;
+
+    int principal = 0;
+    float annualInterest = 0;
+    double monthlyInterest = 0;
+    int period = 0;
+    int numberOfPayments = 0;
+
+
     public void annualCalculator() {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        System.out.println("\t\t\t\t\tAnnual Calculator");
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Annual Calculator\nPrincipal: ");
-        int principal = sc.nextInt();
+        this.principalValidator(sc);
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = sc.nextFloat();
-        double monthlyInterest = (annualInterest / PERCENT) / MONTHS_IN_YEAR;
+        this.annualInterestValidator(sc);
 
-        System.out.print("Period (Years): ");
-        int period = sc.nextInt();
-        int numberOfPayments = period * MONTHS_IN_YEAR;
+        this.periodValidator(sc);
 
         double estimate = Math.pow((1 + monthlyInterest), numberOfPayments);
         double nominator = principal * (monthlyInterest * estimate);
 
-        String mortgage = NumberFormat.getCurrencyInstance(Locale.US).format(nominator / (estimate - 1));
+        String mortgage = this.formatPrice(nominator / (estimate - 1));
         System.out.println("Mortgage: " + mortgage);
+    }
+
+    private void principalValidator(Scanner sc) {
+        while (true) {
+            System.out.print("Principal ($1k - $1M): ");
+            principal = sc.nextInt();
+            if (principal > MIN_PRINCIPAL && principal < MAX_PRINCIPAL) break;
+            String minFormat = this.formatPrice(MIN_PRINCIPAL);
+            String maxFormat = this.formatPrice(MAX_PRINCIPAL);
+            System.out.println("Enter a number between " + minFormat + " and " + maxFormat);
+        }
+    }
+
+    private void annualInterestValidator(Scanner sc) {
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = sc.nextFloat();
+            if (annualInterest > 1 && annualInterest < 30) {
+                monthlyInterest = (annualInterest / PERCENT) / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Annual Interest Rate must be between 1 and 30");
+        }
+    }
+
+    private void periodValidator(Scanner sc) {
+        while (true) {
+            System.out.print("Period (Years): ");
+            period = sc.nextInt();
+            if (period > 1 && period < 30) {
+                numberOfPayments = period * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Period must be between 1 and 30");
+        }
+    }
+
+    private String formatPrice(double price) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(price);
     }
 }
 
