ad8681a9561f10cb70d5067e8d1c4b37b7bedd14
LOVELY-WANDYTE\personal <itsoluwatobby@gmail.com>
2024-06-29 04:12:27 +0100

    conditionals

--- firstCode/src/Main.java
+++ firstCode/src/Main.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
@@ -7,29 +6,9 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        final byte MONTHS_IN_YEAR = 12;
-//        final byte PERCENT = 100;
-//        Scanner sc = new Scanner(System.in);
 
-//        System.out.print("Annual Calculator\nPrincipal: ");
-//        int principal = sc.nextInt();
-//
-//        System.out.print("Annual Interest Rate: ");
-//        float annualInterest = sc.nextFloat();
-//        double monthlyInterest = (annualInterest / PERCENT) / MONTHS_IN_YEAR;
-//
-//        System.out.print("Period (Years): ");
-//        int period = sc.nextInt();
-//        int numberOfPayments = period * MONTHS_IN_YEAR;
-//
-//        double estimate = Math.pow((1 + monthlyInterest), numberOfPayments);
-//        double nominator = principal * (monthlyInterest * estimate);
-//
-//        String mortgage = NumberFormat.getCurrencyInstance(Locale.US).format(nominator / (estimate - 1));
-//        System.out.println("Mortgage: " + mortgage);
-
-        Learning learn =  new Learning();
-        learn.learningPeriod();
+        Exercise exercise = new Exercise();
+        exercise.exercise();
     }
 }
 
@@ -43,14 +22,67 @@ class Learning {
 //        boolean hasCriminalRecord = false;
 //        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
 //        System.out.println(isEligible);
-        int temperature = 32;
-        if (temperature > 30) {
-            System.out.println("It's a hot day");
-            System.out.println("It's a hot day");
+//        int temperature = 32;
+//        if (temperature > 30) {
+//            System.out.println("It's a hot day");
+//            System.out.println("It's a hot day");
+//        }
+//        else if (temperature > 20)
+//            System.out.println("Beautiful day");
+//        else
+//            System.out.println("Cold day");
+//        int income = 120_000;
+//        String className = income > 100_000 ? "First Class" : "Economy";
+//        System.out.println(className);
+
+        //  SWITCH STATEMENT
+        String role = "";
+        switch (role) {
+            case "admin":
+                System.out.println("You're an Admin");
+                break;
+            case "moderator":
+                System.out.println("You're a Moderator");
+                break;
+            default:
+                System.out.println("You are a guest");
         }
-        else if (temperature > 20)
-            System.out.println("Beautiful day");
-        else
-            System.out.println("Cold day");
+    }
+}
+
+class AnnualCalculator {
+    public void annualCalculator() {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Annual Calculator\nPrincipal: ");
+        int principal = sc.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = sc.nextFloat();
+        double monthlyInterest = (annualInterest / PERCENT) / MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        int period = sc.nextInt();
+        int numberOfPayments = period * MONTHS_IN_YEAR;
+
+        double estimate = Math.pow((1 + monthlyInterest), numberOfPayments);
+        double nominator = principal * (monthlyInterest * estimate);
+
+        String mortgage = NumberFormat.getCurrencyInstance(Locale.US).format(nominator / (estimate - 1));
+        System.out.println("Mortgage: " + mortgage);
+    }
+}
+
+class Exercise {
+    public void exercise() {
+        System.out.print("FizzBuzz\nEnter number: ");
+        Scanner sc = new Scanner(System.in);
+        short userInput = sc.nextShort();
+        if (userInput % 5 == 0 && userInput % 3 == 0) System.out.println("FizzBuzz");
+        else if (userInput % 5 == 0) System.out.println("Fizz");
+        else if (userInput % 3 == 0) System.out.println("Buzz");
+        else System.out.println(userInput);
     }
 }
