be804f387093e92d1922c39e3b9d09c22b3fa856
LOVELY-WANDYTE\personal <itsoluwatobby@gmail.com>
2024-07-11 19:31:24 +0100

    done

--- firstCode/src/collections/Array.java
+++ firstCode/src/collections/Array.java
@@ -1,9 +1,6 @@
 package collections;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Array {
